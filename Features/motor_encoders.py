d88ccfcfd4b0ad0a927715a7bacf013fd459e95c
abc <abe@example.com>
2020-11-06 09:15:27 -0800

    Updated motor/encoder testing program

--- Features/motor_encoders.py
+++ Features/motor_encoders.py
@@ -1,1 +1,158 @@
-print("new file")
+from __future__ import division
+import Adafruit_PCA9685
+import RPi.GPIO as gpio
+import time
+import serial
+
+# Initialize PCA9685 Board
+pwm = Adafruit_PCA9685.PCA9685(address=0x40, busnum=0)
+
+#initiate gpio pins & ignore warnings
+gpio.setmode(gpio.BOARD)
+gpio.setwarnings(False)
+
+#Inverse direction pin (low=forward, high=backward)
+inv = 31
+gpio.setup(inv,gpio.OUT)
+
+# RIGHT WHEEL
+#motor A initialize
+#in1 and in2
+m1_in1 = 19
+m1_in2 = 21
+gpio.setup(m1_in1,gpio.OUT)
+gpio.setup(m1_in2,gpio.OUT)
+
+# LEFT WHEEL
+#motor B initialize
+#in1 and in2
+m2_in1 = 29
+m2_in2 = 23
+gpio.setup(m2_in1,gpio.OUT)
+gpio.setup(m2_in2,gpio.OUT)
+
+# PWM pins
+pwm1 = 8
+pwm2 = 9
+pwm.set_pwm(pwm1,0,2000)
+pwm.set_pwm(pwm2,0,2000)
+
+# Arduino Communication
+SERIAL_PORT = '/dev/ttyACM0'
+BAUD_RATE = 230400
+ser = serial.Serial(SERIAL_PORT,BAUD_RATE)
+
+def encoder_pid():
+    p = []
+    while True:
+        data = ser.readline()
+        decoded_bytes = int(data[0:len(data)-2].decode("utf-8"))
+        if data:
+            p.append(decoded_bytes)
+        if len(p) == 2:
+            enc1 = p[0]
+            enc2 = p[1]
+            print(enc1)
+            print(enc2)
+        if len(p) == 2:
+            p = []
+
+def encoder_read():
+    s = []
+    while True:
+        data = ser.readline()
+        decoded_bytes = int(data[0:len(data)-2].decode("utf-8"))
+        if data:
+            s.append(decoded_bytes)
+        if len(s) == 2:
+            enc1 = s[0]
+            enc2 = s[1]
+            print(enc1)
+            print(enc2)
+            if enc1 > 3592 or enc2 > 3592:
+                stop()
+                print("Stop")
+                break
+        if len(s) == 2:
+            print(s)
+            s = []
+
+def backward():
+    #motor A
+    gpio.output(m1_in1,gpio.LOW)
+    gpio.output(m1_in2,gpio.HIGH)
+    #motor B
+    gpio.output(m2_in1,gpio.LOW)
+    gpio.output(m2_in2,gpio.HIGH)
+    gpio.output(inv,gpio.HIGH)
+
+def forward():
+    #motor A
+    gpio.output(m1_in1,gpio.LOW)
+    gpio.output(m1_in2,gpio.HIGH)
+    #motor B
+    gpio.output(m2_in1,gpio.LOW)
+    gpio.output(m2_in2,gpio.HIGH)
+    gpio.output(inv,gpio.LOW)
+
+def stop():
+    #motor A
+    gpio.output(m1_in1,gpio.LOW)
+    gpio.output(m1_in2,gpio.LOW)
+    #motor B
+    gpio.output(m2_in1,gpio.LOW)
+    gpio.output(m2_in2,gpio.LOW)
+
+def left():
+    #motor A
+    gpio.output(m1_in1,gpio.HIGH)
+    gpio.output(m1_in2,gpio.LOW)
+    #motor B
+    gpio.output(m2_in1,gpio.LOW)
+    gpio.output(m2_in2,gpio.HIGH)
+    gpio.output(inv,gpio.HIGH)
+
+def right():
+    #motor A
+    gpio.output(m1_in1,gpio.HIGH)
+    gpio.output(m1_in2,gpio.LOW)
+    #motor B
+    gpio.output(m2_in1,gpio.LOW)
+    gpio.output(m2_in2,gpio.HIGH)
+    gpio.output(inv,gpio.LOW)
+
+print("\n")
+print("Motor Tester...Press spacebar to quit\n")
+print("Use the following 'wasd' keys to control the robot: \n")
+print("Forward - w  Backward - s  Right - d  Left - a  Stop - q")
+print("\n")
+
+while True:
+    value = input()
+    if value == 'w':
+        print("Forward")
+        #forward()
+        print("\n")
+        encoder_pid()
+    elif value == 's':
+        backward()
+        print("Backward")
+        print("\n")
+    elif value == 'd':
+        print("Right")
+        right()
+        print("\n")
+    elif value == 'a':
+        print("Left")
+        left()
+        print("\n")
+    elif value == 'q':
+        print("Stop")
+        stop()
+        print("\n")
+    elif value == ' ':
+        break
+    else:
+        print("<<< Invalid Key Entry >>>")
+        print("\n")
+
