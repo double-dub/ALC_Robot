711fe21cc1f413c2e35a27246a115547763c6e7f
double-dub <55034595+double-dub@users.noreply.github.com>
2020-11-08 16:01:25 -0800

    Update manipulator.py
    testing staging pull then push

--- Features/manipulator.py
+++ Features/manipulator.py
@@ -1,4 +1,4 @@
-# Simple demo of of the PCA9685 PWM servo/LED controller library.
+# test Simple demo of of the PCA9685 PWM servo/LED controller library.
 # This will move channel 0 from min to max position repeatedly.
 # Author: Tony DiCola
 # License: Public Domain
