d7133c8b359d5852101ae5e8ce6f5f1ce3fc9692
abc <abe@example.com>
2020-11-13 16:33:55 -0800

    manipulator calibrating

--- Features/manipulator.py
+++ Features/manipulator.py
@@ -1,12 +1,21 @@
-# Simple demo of of the PCA9685 PWM servo/LED controller library.
+# Simple dem  of of the PCA9685 PWM sdge for HL-L2320Dtoner cartridge for HL-L2320Dtoner cartridge for HL-L2320Dtoner cartridge for HL-L2320Dtoner cartridge for HL-L2320Dtoner cartridge for HL-L2320Dervo/LED controller library.
 # box Simple demo of of the PCA9685 PWM servo/LED controller library.
 # Simple demo of of the PCA9685 PWM servo/LED controller library.
+
 # This will move channel 0 from min to max position repeatedly.
+#to turn off servo use pwm.set_pwm(channel, 4096, 0)
+#turn on servo fully pwm.set_pwm(channel, 0, 4096)
 from __future__ import division
 import time
 
 # Import the PCA9685 module.
 import Adafruit_PCA9685
+import RPi.GPIO as gpio
+
+#initiate gpio pins & ignore warnings
+gpio.setmode(gpio.BOARD)
+gpio.setwarnings(False)
+
 
 
 # Uncomment to enable debug output.
@@ -49,133 +58,154 @@ def rest():
     #keeps base joint straight forward
     #pwm.set_pwm(0, 0, servo_min)
     #Joint one at resting
-    pwm.set_pwm(11, 0, 200)
+    pwm.set_pwm(10, 0, 650)
+    pwm.set_pwm(11, 0, 650)
     #Joint two at resting
-    pwm.set_pwm(12, 0, 150)
+    pwm.set_pwm(12, 0, 650)
     #Joint three gripper at resting
-    pwm.set_pwm(14, 0, 400)
-    time.sleep(1)
-    pwm.set_pwm(14, 4095, 0)
-
+    pwm.set_pwm(14, 0, 300)
+    time.sleep(.5)
+    dig_stop(14)
+    stop(10)
 def extend():
     #pwm.set_pwm(1, 0, servo_min)
     #pwm.set_pwm(2, 0, servo_min)
-    i = 150
-    j = 150
-    while (150<=i<=600):
+    i = 650
+    j = 650
+    while (150<=i<=650):
         
   
-        if (150<=j<=290):
-            pwm.set_pwm(11, 0, i)
-            pwm.set_pwm(12, 0, j)
-            j+=1
-            i+=1
-            print("i=:",i)
-            print("j=:",j)
-            time.sleep(.009)
-        else:
-            #whichever joint is still moving after one of them stops
-            pwm.set_pwm(11, 0, i)
-            print("i=:",i)
-            print("j=:",j)
-            i +=1
-            time.sleep(.009)
-        if i == 600:
-            print("test")
-            break
-
-def grab():
-    #sets servo angle of gripper to squeeze object
-    time.sleep(1)
-    pwm.set_pwm(14, 0, 400)
-    time.sleep(1)
-    pwm.set_pwm(14, 0, 500)
-    print("grab")
-    time.sleep(1)
-    pwm.set_pwm(14, 4095, 0)
-
-def retrieve():
-    #work backwards from grab positions
-    i = 600
-    j = 290
-    while (150<=i<=600):
-        
-  
-        if (150<=j<=290):           
+        if (425<=j<=650):
             pwm.set_pwm(11, 0, i)
             pwm.set_pwm(12, 0, j)
             j-=1
             i-=1
             print("i=:",i)
             print("j=:",j)
-            time.sleep(.009)
+            time.sleep(.002)
         else:
             #whichever joint is still moving after one of them stops
             pwm.set_pwm(11, 0, i)
             print("i=:",i)
             print("j=:",j)
             i -=1
-            time.sleep(.009)
+            time.sleep(.002)
         if i == 150:
+            print("test")
+            break
+
+def grab():
+    #sets servo angle of gripper to squeeze object
+    time.sleep(.5)
+    pwm.set_pwm(14, 0, 300)
+    time.sleep(1)
+    pwm.set_pwm(14, 0, 525)
+    time.sleep(1)
+    #pwm.set_pwm(14, 4095, 0)
+    #time.sleep(.5)
+def retrieve():
+    #work backwards from grab positions
+    i = 150
+    j = 400
+    while (150<=i<=650):
+        
+  
+        if (400<=j<=650):           
+            pwm.set_pwm(11, 0, i)
+            pwm.set_pwm(12, 0, j)
+            j+=1
+            i+=1
+            print("i=:",i)
+            print("j=:",j)
+            time.sleep(.002)
+        else:
+            #whichever joint is still moving after one of them stops
+            pwm.set_pwm(11, 0, i)
+            print("i=:",i)
+            print("j=:",j)
+            i +=1
+            time.sleep(.002)
+        if i == 650:
             print("retrieve")
             break
+
+def spindrop():
+    pwm.set_pwm(10, 0, 650)
+    pwm.set_pwm(10, 0, 125)
+    time.sleep(1)
+    i = 650
+    j = 650
+    while (400<=i<=650):
+        
+  
+        if (550<=j<=650):
+            pwm.set_pwm(11, 0, i)
+            pwm.set_pwm(12, 0, j)
+            j-=1
+            i-=1
+            print("i=:",i)
+            print("j=:",j)
+            time.sleep(.002)
+        else:
+            #whichever joint is still moving after one of them stops
+            pwm.set_pwm(11, 0, i)
+            print("i=:",i)
+            print("j=:",j)
+            i -=1
+            time.sleep(.002)
+        if i == 150:
+            print("test")
+            break
+    
+    print("release")
+    pwm.set_pwm(14, 0, 250)
+    time.sleep(1)
+    #pwm.set_pwm(10, 4096, 0)
+    stop(10)
+    stop(14)
+
+def stop(ch):
+    pwm.set_pwm(ch, 4096, 0)
+def dig_stop(ch):
+    pwm.set_pwm(ch, 4095, 0)
+    time.sleep(.5)
 
 #thewse commands used in order
 rest()
 extend()
 grab()
 retrieve()
-
-'''
+spindrop()
+rest()
 while True:
     value = input()
-    if value == 'w':
-        print("Forward")
-       
-        #extend()
+    if value == 'z':
+        print("rest")
+        rest()
         
-        pwm.set_pwm(11, 4095, 0)
-        pwm.set_pwm(12, 4095, 0)
-        pwm.set_pwm(14, 4095, 0)
+    elif value == 'x':
+        print("extend")
+        extend()
 
+    elif value == 'c':
+        print("grab")
+        grab()
+        time.sleep(1)
+        dig_stop(14)
+        #pwm.set_pwm(14, 0, 4096)
+        #time.sleep(1)
+        #stop(14)
+
+    
+    elif value == 'v':
+        print("retrieve")
+        retrieve()
         
-        #pwm.set_pwm(14, 0, 150)
-        time.sleep(2)
-        #sends servo a low signal so that it relaxes the servo
-        #pwm.set_pwm(14, 0, 400)
-        time.sleep(2)
-        #pwm.set_pwm(13, 4095, 0)
-        print("\n")
-    elif value == 's':
-        #backward()
-        pwm.set_pwm(14, 0, 150)
-        time.sleep(2)
-        pwm.set_pwm(14, 0, 600)
-        time.sleep(2)
-
-        print("Backward")
-        print("\n")
-        
-    elif value == 'd':
-        print("Right")
-        #right()
-        pwm.set_pwm(15, 0, 150)
-        time.sleep(2)
-        pwm.set_pwm(15, 0, 600)
-        time.sleep(2)
-
-        print("\n")
-    elif value == 'a':
-        print("Left")
-        left()
-        print("\n")
-    elif value == 'q':
-        print("Stop")
-        stop()
-        print("\n")
+    elif value == 'b':
+        print("spin and drop")
+        spindrop()
     elif value == ' ':
         break
     else:
         print("<<< Invalid Key Entry >>>")
         print("\n")
-    '''
