f135e4dcc3da9c30892d79e8e250e8976d7940ba
Albert <a.andrade332@gmail.com>
2020-11-21 01:17:18 -0800

    added comments and function now handles deg-rad conversions

--- IMU_Files/calc_avg_angle.py
+++ IMU_Files/calc_avg_angle.py
@@ -2,22 +2,35 @@ from array import *
 import math
 
 # Sample array to compare to standard mean
-angle_values1 = array('f', [math.radians(0.0), math.radians(30.0),
-                            math.radians(60.0), math.radians(90.0)])
+angle_values1 = array('f', [0.0, 30.0, 60.0, 90.0])
+
 # Sample array to compare with 360-deg values
-angle_values2 = array('f', [math.radians(0.0), math.radians(270.0),
-                            math.radians(300.0), math.radians(330.0)])
+angle_values2 = array('f', [0.0, 270.0, 300.0, 330.0])
 
-sin_sum = 0
-cos_sum = 0
 
-for i in angle_values2:
-    sin_sum += math.sin(i)
-    cos_sum += math.cos(i)
+# This function takes in a list of angles. Converts angles to radians
+# in order to use trig functions.
+# Parameter: a list of float values, angles should be in degrees
+# Return: mean of circular quantities in degrees
+def calc_avg_angle(float_arr):
+    for x in range(len(float_arr)):
+        float_arr[x] = math.radians(float_arr[x])
 
-avg_angle = math.degrees(math.atan(sin_sum / cos_sum))
+    sin_sum = 0
+    cos_sum = 0
 
-if avg_angle < 0:
-    avg_angle += 360
+    for i in float_arr:
+        sin_sum += math.sin(i)
+        cos_sum += math.cos(i)
 
-print("Average angle is: ", avg_angle)
+    avg_angle = math.degrees(math.atan(sin_sum / cos_sum))
+
+    # Negative angles are converted to their positive equivalent
+    if avg_angle < 0:
+        avg_angle += 360
+
+    return avg_angle
+
+
+print("Average angle is: ", calc_avg_angle(angle_values1))
+print("Average angle is: ", calc_avg_angle(angle_values2))
